2f0108339e1cf9887daa124a77cecf10ff7b937b
Ramez Al-Siddiq <ramez.sydney@gmail.com>
2023-05-13 22:43:44 +1000

    created the components in react and imported them into the App.jsx file

---
+++ scuba-dive-log/src/components/Sign_Up.jsx
@@ -0,0 +1,9 @@
+const Sign_Up = () => {
+    return (
+        <div>
+            <h1>Sign-up coming soon</h1>
+        </div>
+    );
+};
+
+export default Sign_Up;
---
+++ scuba-dive-log/src/components/Update.jsx
@@ -0,0 +1,9 @@
+const Update = () => {
+    return (
+        <div>
+            <h1>Update coming soon</h1>
+        </div>
+    );
+};
+
+export default Update;
