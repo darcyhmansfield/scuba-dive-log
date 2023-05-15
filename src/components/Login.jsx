432789cf08b4295a768f7c905880fadfd7ef9b74
darcyhmansfield <darcyhmansfield@gmail.com>
2023-05-15 10:29:09 +1000

    updates to components

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,13 +3,8 @@ import {
   // Import predefined theme
   ThemeSupa,
 } from '@supabase/auth-ui-shared'
-import { createClient } from '@supabase/supabase-js';
 import { useNavigate } from 'react-router-dom';
-
-const supabase = createClient(
-  'https://wgpigjfrywpnomybyqqj.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndncGlnamZyeXdwbm9teWJ5cXFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM4NjI2MzcsImV4cCI6MTk5OTQzODYzN30.NIcn8d3rC_BbdqKM65LNufeAUWIPzeJJgvB7JDKA5ZU'
-)
+import { supabase } from '/src/config/supabaseClient'
 
 const Login = () => {
 
